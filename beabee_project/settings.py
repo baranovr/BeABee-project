b16f8eb4d7db99837d4b5c453ebeacd5afb4d78f
baranovr <rusipbox@gmail.com>
2024-10-19 01:14:31 +0300

    fixed some missprints

--- beabee_project/settings.py
+++ beabee_project/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
     'drf_spectacular',
     'debug_toolbar',
     'beabee',
