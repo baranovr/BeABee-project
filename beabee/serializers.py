1430391c518e6a7d43c63b8b3b1397cee4b13b87
baranovr <rusipbox@gmail.com>
2024-10-18 23:36:13 +0300

    extended amount of beabee serializers

--- beabee/serializers.py
+++ beabee/serializers.py
@@ -12,6 +12,18 @@ class TagSerializer(serializers.ModelSerializer):
         )
 
 
+class TagListSerializer(TagSerializer):
+    class Meta:
+        model = Tag
+        fields = TagSerializer.Meta.fields
+
+
+class TagDetailSerializer(TagSerializer):
+    class Meta:
+        model = Tag
+        fields = TagSerializer.Meta.fields
+
+
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
@@ -23,6 +35,18 @@ class PostSerializer(serializers.ModelSerializer):
             "created_at",
             "tags"
         )
+
+
+class PostListSerializer(PostSerializer):
+    class Meta:
+        model = Post
+        fields = PostSerializer.Meta.fields
+
+
+class PostDetailSerializer(PostSerializer):
+    class Meta:
+        model = Post
+        fields = PostSerializer.Meta.fields
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -37,12 +61,36 @@ class CommentSerializer(serializers.ModelSerializer):
         )
 
 
+class CommentListSerializer(CommentSerializer):
+    class Meta:
+        model = Comment
+        fields = CommentSerializer.Meta.fields
+
+
+class CommentDetailSerializer(CommentSerializer):
+    class Meta:
+        model = Comment
+        fields = CommentSerializer.Meta.fields
+
+
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = (
             "id", "name"
         )
+
+
+class SubjectListSerializer(SubjectSerializer):
+    class Meta:
+        model = Subject
+        fields = SubjectSerializer.Meta.fields
+
+
+class SubjectDetailSerializer(SubjectSerializer):
+    class Meta:
+        model = Subject
+        fields = SubjectSerializer.Meta.fields
 
 
 class TeacherSerializer(serializers.ModelSerializer):
@@ -53,12 +101,36 @@ class TeacherSerializer(serializers.ModelSerializer):
         )
 
 
+class TeacherListSerializer(TeacherSerializer):
+    class Meta:
+        model = Teacher
+        fields = TeacherSerializer.Meta.fields
+
+
+class TeacherDetailSerializer(TeacherSerializer):
+    class Meta:
+        model = Teacher
+        fields = TeacherSerializer.Meta.fields
+
+
 class HomeworkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Homework
         fields = (
             "id", "title", "description", "file", "subject", "teacher", "created_at"
         )
+
+
+class HomeworkListSerializer(HomeworkSerializer):
+    class Meta:
+        model = Homework
+        fields = HomeworkSerializer.Meta.fields
+
+
+class HomeworkDetailSerializer(HomeworkSerializer):
+    class Meta:
+        model = Homework
+        fields = HomeworkSerializer.Meta.fields
 
 
 class StorySerializer(serializers.ModelSerializer):
@@ -74,6 +146,18 @@ class StorySerializer(serializers.ModelSerializer):
         )
 
 
+class StoryListSerializer(StorySerializer):
+    class Meta:
+        model = Story
+        fields = StorySerializer.Meta.fields
+
+
+class StoryDetailSerializer(StorySerializer):
+    class Meta:
+        model = Story
+        fields = StorySerializer.Meta.fields
+
+
 class NewsSerializer(serializers.ModelSerializer):
     class Meta:
         model = News
@@ -86,7 +170,31 @@ class NewsSerializer(serializers.ModelSerializer):
         )
 
 
+class NewsListSerializer(NewsSerializer):
+    class Meta:
+        model = News
+        fields = NewsSerializer.Meta.fields
+
+
+class NewsDetailSerializer(NewsSerializer):
+    class Meta:
+        model = News
+        fields = NewsSerializer.Meta.fields
+
+
 class ImportantInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = News
         fields = NewsSerializer.Meta.fields
+
+
+class ImportantInfoListSerializer(ImportantInfoSerializer):
+    class Meta:
+        model = News
+        fields = ImportantInfoSerializer.Meta.fields
+
+
+class ImportantInfoDetailDetailSerializer(ImportantInfoSerializer):
+    class Meta:
+        model = News
+        fields = ImportantInfoSerializer.Meta.fields
