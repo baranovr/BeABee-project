22efcb3099e4204a136e06a1b50453f6b67c69ab
baranovr <rusipbox@gmail.com>
2024-11-04 19:13:11 +0200

    fixed missprint

--- beabee/signals.py
+++ beabee/signals.py
@@ -7,4 +7,4 @@ from beabee.models import Exam, Calendar
 @receiver(post_save, sender=Exam)
 def create_calendar_entry(sender, instance, created, **kwargs):
     if created:
-        transaction.on_commit(lambda: Calendar.objects.create(exam=instance.exam, date_time=instance.date_time))
+        transaction.on_commit(lambda: Calendar.objects.create(exam=instance, date_time=instance.date_time))
