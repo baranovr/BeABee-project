6d79c603cd839b5c49430424da3a282a102d8dda
Ruslan Baranov <rusipbox@gmail.com>
2024-12-20 06:22:24 +0200

    deleted useless dirs

--- user/custom_token/token_serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
-
-class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
-    def validate(self, attrs):
-        nickname = attrs.get("nickname")
-        if nickname:
-            attrs['email'] = nickname
-        return super().validate(attrs)
